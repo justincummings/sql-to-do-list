3c0d98d265c64411397bbecb42094e0f9d18a8a6
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-11-19 16:26:51 -0600

    added post route to router

--- server/routes/taskrouter.js
+++ server/routes/taskrouter.js
@@ -8,7 +8,7 @@ const pool = require('../modules/pool');
 //get route for tasks
 router.get('/', (req, res) => {
     console.log('GET /tasks');
-//declaring sqlText with sql command to select all rows, should sort by ascending order
+//declaring sqlText with sql string to select all rows, should sort by ascending order
     const sqlText = 'SELECT * FROM "tasks" ORDER BY "id";';
     //tapping the db for data
     pool.query(sqlText)
@@ -24,4 +24,18 @@ router.get('/', (req, res) => {
 });
 
 //post route for tasks
-router.post('/', )
+router.post('/', (req, res) => {
+    console.log('POST /tasks');
+    console.log('req.body:', req.body);
+    const newTask = req.body;
+    const sqlText = 'INSERT INTO "tasks"("task",) VALUES ($1);';
+    //sql string tapping the db
+    pool.query (sqlText, [newTask.task])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log (`Error adding new task`, error);
+            res.sendStatus(500);
+        });
+}); 
