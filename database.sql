05480cca27eb49a6520281a203298de9e2a23f17
Justin Cummings <justin.lewis.cummings@gmail.com>
2021-11-19 13:50:13 -0600

    added SQL database

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tasks (
+	"id" SERIAL PRIMARY KEY,
+	"task"  varchar (140),
+	"status" boolean DEFAULT FALSE
+);
+
+INSERT INTO "tasks"
+("task", "status")
+VALUES
+('learn to code', false),
+('remind myself everything is fine', false),
+('get amazing new job in tech', false),
+('remind myself everything is fine...again', false),
+('profit.', false);
